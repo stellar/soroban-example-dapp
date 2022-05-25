ca8383c4af5053e5934031e103b4b4786cb944b8
Paul Bellamy <paul@stellar.org>
2022-05-25 14:02:52 +0100

    Working against backend

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,8 @@ import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 // TODO: Use the SDK
-// import StellarSdk from 'stellar-sdk';
+// import * as StellarSdk from 'stellar-sdk';
+import * as StellarBase from 'stellar-base';
 
 const Home: NextPage = () => {
   const [value, setValue] = React.useState<any>(null);
@@ -14,16 +15,27 @@ const Home: NextPage = () => {
     (async () => {
       let url = 'http://localhost:3000/api/horizon';
       // const server = new StellarSdk.Server(url, { allowHttp: true });
-      const owner = 'GDUT3U3X5RID2KKXBF7GGANYH4UT3RUT4Y5KLLGHTAIOJT67UZUNQ4Y2';
-      const id = 1;
-      // TODO: Do the xdr dance here.
-      const key = "AAAABQAAAANmb28A";
+      const contractOwner = 'GDUT3U3X5RID2KKXBF7GGANYH4UT3RUT4Y5KLLGHTAIOJT67UZUNQ4Y2';
+      const contractId = 1;
 
-      const page = await axios.get(url+'/contracts', {
-        params: { owner, id, key },
+      const args: StellarBase.xdr.ScVal[] = [
+        StellarBase.xdr.ScVal.scvPosI64(
+          StellarBase.xdr.Int64.fromString("1")
+        )
+      ];
+      const response = await axios.post(url+'/rpc', {
+        id: 1,
+        method: "call",
+        params: {
+          contract: `${contractOwner}:${contractId}`,
+          func: "invoke",
+          xdr: StellarBase.xdr.ScVec.toXDR(args).toString('base64'),
+        }
       });
-      // TODO: Parse the value_xdr here
-      setValue(page.data?._embedded?.records?.map((r: any) => r.value_xdr));
+      let parsed = response.data?.result ? {
+        result: StellarBase.xdr.ScVal.fromXDR(response.data.result, 'base64').posI64()
+      } : response.data;
+      setValue(parsed);
     })();
   }, []);
 
@@ -36,23 +48,14 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        {value?.map((v: any, key: number) => (
-          <div key={key}>{JSON.stringify(v)}</div>
-        ))}
+        {!value ? (
+          <div>Loading...</div>
+        ) : value.result ? (
+          <div>Result: {JSON.stringify(value.result)}</div>
+        ) : (
+          <div>Error: {value.error}</div>
+        )}
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
