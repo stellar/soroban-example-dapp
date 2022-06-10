0f56039805c1e1dacb29481128b35a0f3d309201
Paul Bellamy <paul@stellar.org>
2022-06-10 09:51:55 +0100

    extract and document fetching logic

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,47 +7,96 @@ import styles from '../styles/Home.module.css';
 // TODO: Use the SDK
 // import * as StellarSdk from 'stellar-sdk';
 import * as StellarBase from 'stellar-base';
-import init, {invoke_contract} from "../../rs-stellar-wasm-browser/pkg/stellar_wasm_browser.js";
 import { ConnectButton } from "../wallet";
 
+const FACTORIAL_WASM = "AGFzbQEAAAABBgFgAX4BfgMCAQAFAwEAAQYIAX8BQYCABAsHEwIGbWVtb3J5AgAGaW52b2tlAAAKNwE1AQJ+QgAhASAAQgAgAEIAVRshAkIBIQACQANAIAIgAVENASAAIAFCAXwiAX4hAAwACwsgAAs=";
 const PIXEL_NFT_WASM = "AGFzbQEAAAABDgNgAAF+YAF/AGABfwF+AwUEAAABAgUDAQARBhkDfwFBgIDAAAt/AEG8gcAAC38AQcCBwAALBzUFBm1lbW9yeQIABXBpeGVsAAAFb3duZXIAAQpfX2RhdGFfZW5kAwELX19oZWFwX2Jhc2UDAgqYAwQJAELxv+u+lQEL+QICA38CfgJ+IwBBEGsiACQAA0ACQAJAIAACfyACQQpGBEAgAEEIaiAEQgSGQgmENwMAQQAMAQsgAkEKRwRAQgEhAyACQYCAQGstAAAiAUHfAEYNAiABrSEDAkACQCABQTBrQf8BcUEKTwRAIAFBwQBrQf8BcUEaSQ0BIAFB4QBrQf8BcUEaSQ0CIABBATYCBCAAQQhqIAE2AgBBAQwECyADQi59IQMMBAsgA0I1fSEDDAMLIANCO30hAwwCCyAAQQA2AgQgAEEIakEKNgIAQQELNgIADAELIAJBAWohAiADIARCBoaEIQQMAQsLIAAoAgBFBEAgACkDCCAAQRBqJAAMAQsjAEEgayIAJAAgAEEUakEANgIAIABBrIHAADYCECAAQgE3AgQgAEEONgIcIABBjIHAADYCGCAAIABBGGo2AgAjAEEgayIBJAAgAUEBOgAYIAFBnIHAADYCFCABIAA2AhAgAUGsgcAANgIMIAFBrIHAADYCCAALCwMAAQsNAELX1o7QiJnYj7V/CwvDAQEAQYCAwAALuQFHQktMTVFWTkNSL1VzZXJzL3BhdWxiZWxsYW15Ly5jYXJnby9naXQvY2hlY2tvdXRzL3JzLXN0ZWxsYXItY29udHJhY3QtZW52LWE3NDU5OGJlZmVmNTk3OGQvNmIzNmZkNS9zdGVsbGFyLWNvbnRyYWN0LWVudi1jb21tb24vc3JjL3N5bWJvbC5yc2V4cGxpY2l0IHBhbmljAAAKABAAggAAAFoAAAAXAAAAAQAAAAAAAAABAAAAAg==";
+
+// Stub dummy data for now.
+const contractOwner = 'GDUT3U3X5RID2KKXBF7GGANYH4UT3RUT4Y5KLLGHTAIOJT67UZUNQ4Y2';
+const contractId = "0000000000000000000000000000000000000000000000000000000000000000";
+
+// Fetch the result value by making a json-rpc request to an rpc backend.
+async function fetchFromBackend() {
+  let url = '/api/horizon';
+
+  // Example of how to set up the args:
+  // const args: StellarBase.xdr.ScVal[] = [
+  //   StellarBase.xdr.ScVal.scvPosI64(
+  //     StellarBase.xdr.Int64.fromString("3")
+  //   )
+  // ];
+
+  // Pixel NFT "pixel" func takes no args, so we can pass an empty string.
+  const argsXdr = "";
+
+  // Ask the backend to simulate the pixel.pixel func. We could wrap this into
+  // a codegenerated Pixel class, so you'd do:
+  // `new Pixel("owner:address").pixel()`
+  // This could also be part of the stellar-sdk server package. tbd.
+  const response = await axios.post(url+'/rpc', {
+    id: 1,
+    method: "call",
+    params: {
+      contract: `${contractId}`,
+      func: "pixel",
+      xdr: argsXdr,
+    }
+  });
+  if (response.data?.error) {
+    throw response.data?.error;
+  }
+  const resultXdr = response.data?.result;
+
+  // Parse the result u32. Again, could be wrapped into a codegenned helper.
+  return StellarBase.xdr.ScVal.fromXDR(resultXdr, 'base64').u32().toString(16);
+}
+
+// Fetch the result value by running the contract runtime in-browser.
+// Experimental. The catch here is you'd still need to load all the current chain state data.
+async function fetchInBrowser() {
+  // Point this to wherever your rs-stellar-wasm-browser repo is built.
+  const {default: init, invoke_contract} = require("../../rs-stellar-wasm-browser/pkg/stellar_wasm_browser.js");
+
+  // Example of how to set up the args:
+  // const args: StellarBase.xdr.ScVal[] = [
+  //   StellarBase.xdr.ScVal.scvPosI64(
+  //     StellarBase.xdr.Int64.fromString("3")
+  //   )
+  // ];
+
+  // Pixel NFT "pixel" func takes no args, so we can pass an empty string.
+  const argsXdr = "";
+
+  // Initialize the runtime
+  await init();
+
+  // Simulate the pixel.pixel func. We could wrap this into a codegenerated
+  // Pixel class, so you'd do: `new Pixel("owner:address").pixel()`
+  const resultXdr = Buffer.from(invoke_contract(contractId, PIXEL_NFT_WASM, "pixel", argsXdr));
+
+  // Parse the result u32. Again, could be wrapped into a codegenned helper.
+  return StellarBase.xdr.ScVal.fromXDR(resultXdr, 'base64').u32().toString(16);
+}
 
 const Home: NextPage = () => {
   const [value, setValue] = React.useState<any>(null);
   
+  // Simulate the contract and store the result on first page load.
   React.useEffect(() => {
     (async () => {
-      let url = '/api/horizon';
-      // const server = new StellarSdk.Server(url, { allowHttp: true });
-      const contractOwner = 'GDUT3U3X5RID2KKXBF7GGANYH4UT3RUT4Y5KLLGHTAIOJT67UZUNQ4Y2';
-      const contractId = "0000000000000000000000000000000000000000000000000000000000000000";
-
-
-      // const args: StellarBase.xdr.ScVal[] = [
-      //   StellarBase.xdr.ScVal.scvPosI64(
-      //     StellarBase.xdr.Int64.fromString("3")
-      //   )
-      // ];
-
-      // const argsXdr = StellarBase.xdr.ScVec.toXDR(args).toString('base64');
-      const argsXdr = "";
-
-      const response = await axios.post(url+'/rpc', {
-        id: 1,
-        method: "call",
-        params: {
-          contract: `${contractId}`,
-          func: "pixel",
-          xdr: argsXdr,
+      try {
+        let result = await fetchFromBackend();
+        // let result = await fetchInBrowser();
+        setValue({ result });
+      } catch (error) {
+        // stringify any error for display.
+        if (!!(error as any)?.toString) {
+          setValue({ error: (error as any).toString() });
+          return;
         }
-      });
-      console.debug(response.data);
-      const resultXdr = response.data?.result;
-
-      // await init();
-      // const resultXdr = Buffer.from(invoke_contract(contractId, PIXEL_NFT_WASM, "pixel", argsXdr));
-      const result = StellarBase.xdr.ScVal.fromXDR(resultXdr, 'base64').u32().toString(16);
-      setValue({ result });
+        setValue({ error });
+      }
     })();
   }, []);
 
@@ -74,6 +123,7 @@ const Home: NextPage = () => {
                 borderRadius: "10%",
                 margin: "1rem",
               }} />
+              {/* slice here to drop the alpha byte */}
               <span>#{value.result.slice(0,6)}</span>
             </>
           ) : (
