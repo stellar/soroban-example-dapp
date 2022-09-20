059d0a0f19d4411d6c2fbfb755246b551c9e15f6
Siddharth Suresh <siddharth@stellar.org>
2022-09-20 14:38:54 -0700

    remove unused imports

--- contracts/crowdfund/src/testutils.rs
+++ contracts/crowdfund/src/testutils.rs
@@ -1,18 +1,12 @@
 #![cfg(any(test, feature = "testutils"))]
 
 use crate::CrowdfundClient;
-use ed25519_dalek::Keypair;
-use soroban_auth::{Ed25519Signature, Identifier, Signature, SignaturePayload, SignaturePayloadV0};
-use soroban_sdk::testutils::ed25519::Sign;
-use soroban_sdk::{BigInt, BytesN, Env, IntoVal, RawVal, Symbol, Vec};
+
+use soroban_sdk::{BytesN, Env};
 
 pub fn register_test_contract(e: &Env, contract_id: &[u8; 32]) {
     let contract_id = BytesN::from_array(e, contract_id);
     e.register_contract(&contract_id, crate::Crowdfund {});
-}
-
-fn to_ed25519(e: &Env, kp: &Keypair) -> Identifier {
-    Identifier::Ed25519(kp.public.to_bytes().into_val(e))
 }
 
 pub struct Crowdfund {
