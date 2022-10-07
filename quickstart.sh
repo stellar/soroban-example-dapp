40f199bcfb7781ee8e2e2c6028ef40affe25f615
Paul Bellamy <paul@paulbellamy.com>
2022-10-07 07:13:01 -0700

    add quickstart standalone/futurenet flag (#32)

--- quickstart.sh
+++ quickstart.sh
@@ -4,11 +4,26 @@ set -e
 
 REVISION=574897620355e2f7486a8cc5586597babd95c85cfa6f148a06470dc80012c2e2
 
+case "$1" in
+standalone)
+    echo "Using standalone network"
+    ARGS="--standalone"
+    ;;
+futurenet)
+    echo "Using Futurenet network"
+    ARGS="--futurenet"
+    ;;
+*)
+    echo "Usage: $0 standalone|futurenet"
+    exit 1
+    ;;
+esac
+
 docker run --rm -ti \
   --platform linux/amd64 \
   --name stellar \
   -p 8000:8000 \
   stellar/quickstart@sha256:$REVISION \
-  --standalone \
+  $ARGS \
   --enable-soroban-rpc \
   --protocol-version 20
