502dde1cee9afa71511a090f84ec185a5df83e3d
lucasmagnus <lucasmagnusdasilva@gmail.com>
2022-10-07 17:02:24 -0300

    Updated labels

--- components/organisms/campaign/index.tsx
+++ components/organisms/campaign/index.tsx
@@ -9,24 +9,21 @@ export function Campaign() {
   return (
     <div className={styles.content}>
       <h6>Starfund #821</h6>
-      <h1>Planetary research for 10 years</h1>
-      <AuthorInfo author="Stellar Xam" dateTime="21 hours ago" image={avatar} />
+      <h1>Planetary open-source photography</h1>
+      <AuthorInfo author="Sam Stroop" dateTime="21 hours ago" image={avatar} />
       <Image src={picture} width={642} height={294} alt="project image" />
-
       <p>
-        Project Description Lorem ipsum dolor sit amet, consectetur adipiscing
-        elit. Nam dignissim blandit mauris, volutpat eleifend ligula. Etiam
-        rhoncus erat ornare mi tincidunt,
-        <br />
-        <br />
-        Project Description Lorem ipsum dolor sit amet, consectetur adipiscing
-        elit. Nam dignissim blandit mauris, volutpat eleifend ligula. Etiam
-        rhoncus erat ornare mi tincidunt,Project Description Lorem ipsum dolor
-        sit amet, consectetur adipiscing elit. Nam dignissim blandit mauris,
-        volutpat eleifend ligula. Etiam rhoncus erat ornare mi tincidunt,Project
-        Description Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
-        dignissim blandit mauris, volutpat eleifend ligula. Etiam rhoncus erat
-        ornare mi tincidunt,
+        Hello! My name is Sam and I’m working with a team of three other
+        passionate individuals to take photos of the planets in our solar system
+        and make them open-source to the world! We are fascinated by the stars
+        and the system and we think images of these entities should be shared
+        with everyone.
+      </p>
+      <p>
+        Our hope is that by making these open-source, people will be able to use
+        them for personal, educational, and even commercial purposes. We are
+        raising funds to help cover the cost of equipment and time spent on this
+        project. Any amount helps and we are grateful for your support!
       </p>
     </div>
   )
