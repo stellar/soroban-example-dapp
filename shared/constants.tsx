48ddd6805b05165d739cd8919a75f3538d87db5a
Paul Bellamy <paul@stellar.org>
2022-10-10 18:46:10 +0200

    use a randomly generated token admin account instead of the standalone root one

--- shared/constants.tsx
+++ shared/constants.tsx
@@ -1,6 +1,9 @@
+// These were randomly generated from https://laboratory.stellar.org/#account-creator
+const TokenAdmin = 'GDT2NORMZF6S2T4PT4OBJJ43OPD3GPRNTJG3WVVFB356TUHWZQMU6C3U'
+const TokenAdminSecretKey = 'SAKCFFFNCE7XAWYMYVRZQYKUK6KMUCDIINLWISJYTMYJLNR2QLCDLFVT'
+
+// Contract IDs, set up by ./initialize.sh
 const CrowdfundId = process.env.CROWDFUND_ID ?? ''
-const TokenAdmin = 'GBZXN7PIRZGNMHGA7MUUUF4GWPY5AYPV6LY4UV2GL6VJGIQRXFDNMADI'
-const TokenAdminSecretKey = 'SC5O7VZUXDJ6JBDSZ74DSERXL7W3Y5LTOAMRF7RQRL3TAGAPS7LUVG3L'
 const TokenId = process.env.TOKEN_ID ?? ''
 
 const Constants = {
