4c9b914d5d72a0fc8745bed733ab7bcd7ad4a01c
Paul Bellamy <paul@stellar.org>
2022-10-10 19:02:59 +0200

    fix friendbot url for futurenet

--- initialize.sh
+++ initialize.sh
@@ -17,7 +17,7 @@ standalone)
   export SOROBAN_SECRET_KEY="SAKCFFFNCE7XAWYMYVRZQYKUK6KMUCDIINLWISJYTMYJLNR2QLCDLFVT"
 
   echo Fund token admin account from friendbot
-  curl "$SOROBAN_RPC_HOST/friendbot?addr=$TOKEN_ADMIN" && echo "success"
+  curl "$SOROBAN_RPC_HOST/friendbot?addr=$TOKEN_ADMIN"
   ;;
 futurenet)
   echo "Using Futurenet network"
@@ -26,7 +26,7 @@ futurenet)
   export SOROBAN_NETWORK_PASSPHRASE="Test SDF Future Network ; October 2022"
   export SOROBAN_SECRET_KEY="SAKCFFFNCE7XAWYMYVRZQYKUK6KMUCDIINLWISJYTMYJLNR2QLCDLFVT"
   # TODO: Use friendbot to fund the token admin, or figure our token admin here...
-  # curl "$SOROBAN_RPC_HOST/friendbot?addr=$TOKEN_ADMIN" && echo "success"
+  curl "https://friendbot-futurenet.stellar.org/?addr=$TOKEN_ADMIN"
   ;;
 ""|sandbox)
   # no-op
