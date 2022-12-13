7e673de06efdc94704d2f9e8cfbce3959a749efc
esteblock <estebaniglesiasmanriquez@gmail.com>
2022-12-12 21:34:58 -0300

    use docker image for soroban 0.3.3

--- quickstart.sh
+++ quickstart.sh
@@ -21,7 +21,7 @@ docker run --rm -ti \
   --platform linux/amd64 \
   --name stellar \
   -p 8000:8000 \
-  stellar/quickstart:soroban-dev \
+  stellar/quickstart:soroban-dev@sha256:8046391718f8e58b2b88b9c379abda3587bb874689fa09b2ed4871a764ebda27 \
   $ARGS \
   --enable-soroban-rpc \
   --protocol-version 20
