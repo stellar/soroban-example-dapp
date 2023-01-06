4aed72a9d48195c7d832a3fc54d7e6328473334d
George Kudrayvtsev <george@stellar.org>
2023-01-05 18:25:45 -0800

    Revert "Temporarily reset stuff"
    This reverts commit 8c4f38d85c8c9d4ba01b14372910b267098be246.

--- components/molecules/form-pledge/index.tsx
+++ components/molecules/form-pledge/index.tsx
@@ -2,6 +2,7 @@ import React, { FunctionComponent, useState } from 'react'
 import { AmountInput, Button, Checkbox } from '../../atoms'
 import { TransactionModal } from '../../molecules/transaction-modal'
 import styles from './style.module.css'
+import { getPublicKey } from "@stellar/freighter-api"
 import {
   useContractValue,
   useNetwork,
@@ -80,8 +81,6 @@ const FormPledge: FunctionComponent<IFormPledgeProps> = props => {
     )
     const nonce = convert.bigNumberToI128(BigNumber(0))
     const amountScVal = convert.bigNumberToI128(parsedAmount)
-
-    console.log(props.account, sequence, invoker, nonce, amountScVal)
 
     try {
       // FIXME: The `sendTransaction` calls throw an error:
@@ -256,7 +255,7 @@ const FormPledge: FunctionComponent<IFormPledgeProps> = props => {
           let { sequence, balances } = await server.getAccount(Constants.TokenAdmin)
           let adminSource = new SorobanClient.Account(Constants.TokenAdmin, sequence)
 
-          let wallet = await server.getAccount(props.account)
+          let wallet = await getPublicKey().then((pk) => server.getAccount(pk))
           let walletSource = new SorobanClient.Account(wallet.id, wallet.sequence)
 
           //
@@ -271,6 +270,7 @@ const FormPledge: FunctionComponent<IFormPledgeProps> = props => {
           //        so we never know whether or not the user needs a trustline
           //        to receive the minted asset. We just do it unconditionally.
           //
+          // if (balances?.filter(b => (
           if (!balances || balances.filter(b => (
             b.asset_code == symbol && b.asset_issuer == Constants.TokenAdmin
           )).length === 0) {
