d226da49680dcb704a52ca312ef18c4c491062f3
esteblock <estebaniglesiasmanriquez@gmail.com>
2023-05-18 13:56:43 -0300

    remove unused types and provideWalletChains

--- wallet/hooks/useNetwork.tsx
+++ wallet/hooks/useNetwork.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { useSorobanReact } from "@soroban-react/core";
-import { chain } from "../provideWalletChains";
+
+
 
 export function useNetwork() {
-  const { activeChain, server } = useSorobanReact()
+  const { activeChain, server, chains} = useSorobanReact()
   return {
     activeChain,
     server,
-    chains: Object.values(chain),
+    chains
   };
 };
