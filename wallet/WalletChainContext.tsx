238632a001db85f3cc23b4278a92f6b7f5e4a070
esteblock <estebaniglesiasmanriquez@gmail.com>
2023-05-18 13:39:25 -0300

    remove unused useWalletChainsById and useWalletChains function

--- wallet/WalletChainContext.tsx
+++ wallet/WalletChainContext.tsx
@@ -11,19 +11,3 @@ export interface WalletChain {
 };
  
 export const WalletChainContext = React.createContext<WalletChain[]>([]);
-
-export const useWalletChains = () => React.useContext(WalletChainContext);
-
-export const useWalletChainsById = () => {
-  const walletChains = useWalletChains();
-
-  return React.useMemo(() => {
-    const walletChainsById: Record<string, WalletChain> = {};
-
-    walletChains.forEach(rkChain => {
-      walletChainsById[rkChain.id] = rkChain;
-    });
-
-    return walletChainsById;
-  }, [walletChains]);
-};
