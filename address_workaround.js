68c6264c0e4c5d895e814c4eaaf1a78cfbe3be84
Shawn Reuland <shawn@stellar.org>
2023-05-29 21:42:35 -0700

    added comment on workarounds script for address conversion

--- address_workaround.js
+++ address_workaround.js
@@ -1,3 +1,6 @@
+// TODO - remove this workaround when
+// https://github.com/stellar/soroban-tools/issues/661 is resolved.
+
 const SorobanClient = require('soroban-client');
 
 const contractId = process.argv[2] || undefined;
