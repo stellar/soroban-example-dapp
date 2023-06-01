93c84e106bf786c40418871b9b3abc18c9c52319
Shawn Reuland <shawn@stellar.org>
2023-06-01 09:42:16 -0700

    #458: review feedback for preview9

--- address_workaround.js
+++ address_workaround.js
@@ -3,12 +3,7 @@
 
 const SorobanClient = require('soroban-client');
 
-const contractId = process.argv[2] || undefined;
+let contractId = process.argv[2] || undefined;
 if (contractId) {
-  if (SorobanClient.StrKey.isValidContract(contractId)) {
-    console.log(contractId);
-    return;
-  }
-  const buf = Buffer.from(contractId, "hex")
-  console.log(SorobanClient.StrKey.encodeContract(buf));
+  console.log(new SorobanClient.Contract(contractId).contractId());
 } 
--- quickstart.sh
+++ quickstart.sh
@@ -17,7 +17,8 @@ futurenet)
     ;;
 esac
 
-# TODO - set this to Preview9 quickstart image when available.
+# this is set to the quickstart `soroban-dev` image annointed as the release 
+# for a given Soroban Release, it is captured on Soroban Releases - https://soroban.stellar.org/docs/reference/releases 
 QUICKSTART_SOROBAN_DOCKER_SHA=stellar/quickstart:soroban-dev@sha256:57e8ab498bfa14c65595fbb01cb94b1cdee9637ef2e6634e59d54f6958c05bdb
 
 shift
