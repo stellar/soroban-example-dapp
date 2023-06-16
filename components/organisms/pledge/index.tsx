4c86660d2353dd89b2c391bca2b871e183553f83
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2023-06-16 14:54:06 -0400

    feat: move subscribe logic to useSubscription hook
    This is a hook that can be copy/pasted into other React projects that
    need such functionality, to work alongside their generated libraries
    until these generated libraries expose subscription logic natively.

--- components/organisms/pledge/index.tsx
+++ components/organisms/pledge/index.tsx
@@ -1,10 +1,11 @@
-import React, { FunctionComponent, useState, useRef } from 'react'
+import React, { FunctionComponent, useState } from 'react'
 import { Card, ConnectButton, Loading, ProgressBar } from '../../atoms'
 import styles from './style.module.css'
 import { Spacer } from '../../atoms/spacer'
 import { Utils } from '../../../shared/utils'
 import {
-  useAccount
+  useAccount,
+  useSubscription,
 } from '../../../hooks'
 import * as crowdfundContract from 'crowdfund-contract'
 import * as abundanceContract from 'abundance-token'
@@ -13,13 +14,6 @@ import * as SorobanClient from 'soroban-client'
 import { Deposits, FormPledge } from '../../molecules'
 import * as convert from '../../../convert'
 let xdr = SorobanClient.xdr
-
-const pollInterval = 5000
-
-// TODO: update js-soroban-client to include latestLedger
-interface GetEventsWithLatestLedger extends SorobanClient.SorobanRpc.GetEventsResponse {
-  latestLedger?: string;
-}
 
 const Pledge: FunctionComponent = () => {
   const [loadedAt, setLoadedAt] = React.useState<number>(Date.now())
@@ -63,91 +57,14 @@ const Pledge: FunctionComponent = () => {
 
   const [targetReached, setTargetReached] = useState<boolean>(false)
 
-  const subscriptions: {
-    contractId: string
-    topics: string[]
-    cb: (event: { value: { xdr: string } }) => void
-    lastLedgerStart?: number
-    pagingToken?: string
-  }[] = React.useMemo(() => [
-    {
-      contractId: crowdfundContract.CONTRACT_ID_HEX,
-      topics: ['pledged_amount_changed'],
-      cb: (event) => {
-        let eventTokenBalance = xdr.ScVal.fromXDR(event.value.xdr, 'base64')
-        setAbundance({ ...abundance!, balance: convert.scvalToBigInt(eventTokenBalance) })
-      },
-    },
-    {
-      contractId: crowdfundContract.CONTRACT_ID_HEX,
-      topics: ['target_reached'],
-      cb: () => { setTargetReached(true) },
-    },
-  ], [abundance, setTargetReached])
+  useSubscription(crowdfundContract, 'pledged_amount_changed', React.useMemo(() => event => {
+    let eventTokenBalance = xdr.ScVal.fromXDR(event.value.xdr, 'base64')
+    setAbundance({ ...abundance!, balance: convert.scvalToBigInt(eventTokenBalance) })
+  }, [abundance]))
 
-  React.useEffect(() => {
-    let timeoutId: NodeJS.Timer | null = null
-    let stop = false
-
-    async function pollEvents(): Promise<void> {
-      try {
-        for (const subscription of subscriptions) {
-          if (!subscription.lastLedgerStart) {
-            let latestLedgerState = await crowdfundContract.Server.getLatestLedger();
-            subscription.lastLedgerStart = latestLedgerState.sequence
-          } 
-         
-          const subscriptionTopicXdrs: Array<string> = []
-          subscription.topics && subscription.topics.forEach( topic => {
-            subscriptionTopicXdrs.push( xdr.ScVal.scvSymbol(topic).toXDR("base64"));
-          })
-
-          // TODO: use rpc batch for single round trip, each subscription can be one
-          // getEvents request in the batch, is that possible now?
-          let response = await crowdfundContract.Server.getEvents({
-            startLedger: !subscription.pagingToken ? subscription.lastLedgerStart : undefined,
-            cursor: subscription.pagingToken,
-            filters: [
-              {
-                contractIds: [subscription.contractId],
-                topics: [subscriptionTopicXdrs],
-                type: "contract"
-              }  
-            ],
-            limit: 10
-          }) as GetEventsWithLatestLedger;
-       
-          delete subscription.pagingToken;
-          if (response.latestLedger) {
-            subscription.lastLedgerStart = parseInt(response.latestLedger);
-          }
-          response.events && response.events.forEach(event => {
-            try {
-              subscription.cb(event)
-            } catch (error) {
-              console.error("Poll Events: subscription callback had error: ", error);
-            } finally {
-              subscription.pagingToken = event.pagingToken
-            }
-          }) 
-        }   
-      } catch (error) {
-        console.error("Poll Events: error: ", error);
-      } finally {
-        if (!stop) {
-          timeoutId = setTimeout(pollEvents, pollInterval);
-        }
-      }
-    }
-
-    pollEvents();
-
-    return () => {
-      if (timeoutId != null) clearTimeout(timeoutId)
-      stop = true
-    }
-  }, [subscriptions]);
-
+  useSubscription(crowdfundContract, 'target_reached', React.useMemo(() => () => {
+    setTargetReached(true)
+  }, []))
 
   return (
     <Card>
